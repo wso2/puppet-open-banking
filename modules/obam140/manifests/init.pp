f1ba8c0e738ff86220fff79012fb885b8c647d2b
KdanW <kalana.16@cse.mrt.ac.lk>
2019-08-06 20:48:32 +0530

    added basic templates

--- modules/obam140/manifests/init.pp
+++ modules/obam140/manifests/init.pp
@@ -1,5 +1,7 @@
 class obam140 inherits obam140::params{
 
+  /* From apim::common */
+
   package { 'unzip': 
     ensure => installed
   }
@@ -18,12 +20,13 @@ class obam140 inherits obam140::params{
     gid     => $user_group_id,
     home    => "/home/${user}",
     system  => true,
-    require => Group["${user_group}"]
+    require => Group["${user_group}"],
+    notify => File["jdk-distribution"],
   }
 
   # Copy JDK to Java distribution path
   file { "jdk-distribution":
-    path => "${java_dir}/${jdk_name}",
+    path => "${java_home}.tar.gz",
     source => "puppet:///modules/${module_name}/jdk/${jdk_name}.tar.gz", #need to add jdk to the master
     notify => Exec["unpack-jdk"],
   }
@@ -35,6 +38,17 @@ class obam140 inherits obam140::params{
     cwd     => "${java_dir}",
     onlyif  => "/usr/bin/test ! -d ${java_home}",
   }
+
+  # Create symlink to Java binary
+  file { "${java_symlink}":
+    ensure  => "link",
+    target  => "${java_home}",
+    require => Exec["unpack-jdk"]
+  }
+
+  
+
+  
 
   /*
   * WSO2 Distribution
@@ -69,9 +83,45 @@ class obam140 inherits obam140::params{
     cwd     => "${pack_dir}",
   }
 
+  # Copy the unit file required to deploy the server as a service
+  file { "/etc/systemd/system/${wso2_service_name}.service":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0754',
+    content => template("${module_name}/carbon.service.erb"),
+  }
+
+  exec { 'systemctl daemon-reload':
+    path        => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+    subscribe   => File["/etc/systemd/system/${wso2_service_name}.service"],
+    refreshonly => true,
+  }
+
+
+  /* From apim */
+
+
+  # Copy wso2server.sh to installed directory
+  file { "${carbon_home}/${start_script_template}":
+    ensure  => file,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0754',
+    content => template("${module_name}/carbon-home/${start_script_template}.erb"),
+    notify  => Service["${wso2_service_name}"],
+#    require => Class["apim_common"]           //original 
+    require => Exec["unzip-update"],
+  }
 
 
 
+ /* from service.pp */
+
+ service { "${wso2_service_name}":
+    enable => true,
+    ensure => running,
+  }
 
 
 }
--- modules/obam140/manifests/params.pp
+++ modules/obam140/manifests/params.pp
@@ -1,24 +1,42 @@
 
 class obam140::params {
 
+  $jvmxms = '2048m'
+  $jvmxmx = '2048m'
 
+  $version = "1.3.0"
 
+  # User details
   $user = 'wso2carbon'
   $user_group = 'wso2'
   $user_id = 802
   $user_group_id = 802
 
+  # JDK Distributions
   $java_dir = "/opt"
-  $jdk_name = "jdk-8u161-linux-x64"
-
-  
+  $jdk_name = "jdk1.8.0_161"
+  $java_symlink = "${java_dir}/java"
+  $java_home = "${java_dir}/${jdk_name}"
 
   $target = "/mnt"
   $product_dir = "${target}/${profile}"
   $pack_dir = "${target}/${profile}/packs"
 
-
-  $version = "1.3.0"
+  $wso2_service_name = "wso2${profile}"
+  
   $pack = "wso2-obam-${version}"
+
+  # Pack Directories
+  $carbon_home = "${product_dir}/${pack}"
   $product_binary = "${pack}.zip"
+
+
+  #from apim
+
+  $start_script_template = 'bin/wso2server.sh'
+
+  #params for carbon.service.erb
+
+  $server_script_path ="${product_dir}/${pack}/bin/wso2server.sh"
+  $pid_file_path="${product_dir}/${pack}/wso2carbon.pid"
 }
